5714b965ced2b648a74fb5823919d61b0a7b32a0
SimonHanzal <71489526+SimonHanzal@users.noreply.github.com>
2020-10-29 09:26:11 +0000

    Update sart_block_2.js

--- sart_block_2.js
+++ sart_block_2.js
@@ -1,13 +1,13 @@
 	/* METADATA */
 	
 	var timeline = [];
-	var no_trials = 27;
+	var no_trials = 1;
 
     /* WELCOME */
     
 	var welcome = {
       type: "html-keyboard-response",
-      stimulus: '<div style = "font-size:22px;">This is the second half of the experiment. Press any key to continue.</div>'
+      stimulus: '<div style = "font-size:22px;">This is the second half of the experiment. Click this text, then press any key to continue.</div>'
     };
     timeline.push(welcome);
 
