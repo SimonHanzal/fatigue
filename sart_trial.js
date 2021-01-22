7a53c4d564129e63f2e1a0380b3e2c4e09c59b1c
Simon Hanzal <71489526+SimonHanzal@users.noreply.github.com>
2021-01-22 10:08:34 +0000

    Update sart_trial.js

--- sart_trial.js
+++ sart_trial.js
@@ -154,9 +154,9 @@
 					"<br>" +
 					'<div style = "font-size:19px;">You missed '+incorrect_go_p+' of the numbers (1, 2, 4, 5, 6, 7, 8, 9) and incorrectly reacted  '+incorrect_no_go_p+'x  to number 3.</div>'+
 					"<br>" +
-					'<div style = "font-size:19px;"> Your accuracy when you were supposed to press the spacebar for numbers was '+accuracy_go+'% and when you were supposed not to press anyhintg was '+accuracy_nogo+'%. </div>'+
+					'<div style = "font-size:19px;"> Your accuracy when you were supposed to press the spacebar was '+accuracy_go+'% and when you were supposed not to press anyhintg was '+accuracy_nogo+'%. </div>'+
 					"<br>" +
-					'<div style = "font-size:19px;"> <b> Please, make sure to continue only if your getting at least some correct responses. </b> </div>'+
+					'<div style = "font-size:19px;"> <b> To receive a payment you must consistently show at least some correct responses to all of the numbers. </b> </div>'+
 					"<br>" +
 					'<div style = "font-size:19px;">Now, press any key to continue to the recorded part of the experiment.</div>';
 		}
