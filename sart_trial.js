993dc014966f481d4a22fe242a2fbb1fa1047eab
Simon Hanzal <71489526+SimonHanzal@users.noreply.github.com>
2021-06-10 21:01:59 +0100

    Update sart_trial.js

--- sart_trial.js
+++ sart_trial.js
@@ -76,8 +76,8 @@
 		  data.go = 2;
 	       }
 	},
-      on_start: function() {
-      setTimeout(function () => { alert("Hello") }, 2000)
+      on_load: function() {
+      setTimeout(function () => { trial.stimulus = '<div style="font-size:64px;">3</div>' }, 1500)
       }
     }
 
